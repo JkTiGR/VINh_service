be6484b25ed08bd6148e09fc0e8268d411de8f6d
JkTiGR <jackietv2015@gmail.com>
2025-03-02 19:23:43 +0200

    Ваше описание изменений

--- app.py
+++ app.py
@@ -170,3 +170,14 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(host="0.0.0.0", port=5003, debug=True)
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify(error="Resource not found"), 404 if request.is_json else render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return jsonify(error="Internal server error"), 500 if request.is_json else render_template('500.html'), 500
+
